ba1ea8d069e1d653553b3c8096a66c2d65379f68
TripodGG <157990933+TripodGG@users.noreply.github.com>
2024-03-06 12:51:24 -0600

    Update Azure AD & Exchange Online.ps1
    start of script

---
+++ Azure AD & Exchange Online.ps1
@@ -0,0 +1,6 @@
+# Name: Azure AD and Exchange Online tasks
+# Author: TripodGG
+# Purpose: Written to perform common tasks within Azure AD (Encarta) and Exchange Online
+# License: MIT License, Copyright (c) 2024 TripodGG
+
+# Define parameters
