d4630b579ed5d90cfe8a9a0c8d71b14f5f9eb609
TripodGG <157990933+TripodGG@users.noreply.github.com>
2024-03-07 09:32:21 -0600

    Update Clear Browser Cache.ps1
    removed prompts as I created a separate script with this function.  This one is for silent clearing of cache

--- Clear Browser Cache.ps1
+++ Clear Browser Cache.ps1
@@ -3,8 +3,7 @@
 # Purpose: Search for all installed web browsers and clear all cached data
 # License: MIT License, Copyright (c) 2024 TripodGG
 
-
-#Function to clear browser cache
+# Function to check for installed browsers and clear cached data
 function Clear-BrowserCaches {
     param()
 
@@ -14,40 +13,6 @@ function Clear-BrowserCaches {
             Write-Host "$browserName cache cleared successfully."
         } else {
             Write-Host "$browserName not detected."
-        }
-    }
-
-    function Confirm-ClearAllCaches {
-        $clearAllCaches = Read-Host -Prompt "Do you want to clear cache for all installed browsers? (Y/N)"
-        if ($clearAllCaches -eq 'Y' -or $clearAllCaches -eq 'yes') {
-            foreach ($browser in $browsers) {
-                Clear-BrowserCache -browserName $browser.Name -cachePath $browser.CachePath
-            }
-            Write-Host "All installed browsers cache has been cleared."
-        } elseif ($clearAllCaches -eq 'N' -or $clearAllCaches -eq 'no') {
-            $confirmSkip = Read-Host -Prompt "Are you sure you don't want to clear any browser cache? (Y/N)"
-            if ($confirmSkip -eq 'Y' -or $confirmSkip -eq 'yes') {
-                $confirmReallySkip = Read-Host -Prompt "Are you REALLY sure you don't want to clear any browser cache? (Y/N)"
-                if ($confirmReallySkip -eq 'Y' -or $confirmReallySkip -eq 'yes') {
-                    $confirmReallyReallySkip = Read-Host -Prompt "Are you *REALLY* REALLY sure you don't want to clear any browser cache? (Y/N)"
-                    if ($confirmReallyReallySkip -eq 'Y' -or $confirmReallyReallySkip -eq 'yes') {
-                        Write-Host "Really?! Why did you run this script?"
-                        exit
-                    } elseif ($confirmReallyReallySkip -eq 'N' -or $confirmReallyReallySkip -eq 'no') {
-                        # Return to the original prompt
-                        Confirm-ClearAllCaches
-                    }
-                } elseif ($confirmReallySkip -eq 'N' -or $confirmReallySkip -eq 'no') {
-                    # Return to the original prompt
-                    Confirm-ClearAllCaches
-                }
-            } elseif ($confirmSkip -eq 'Y' -or $confirmSkip -eq 'yes') {
-                Write-Host "Why did you run this script?"
-                exit
-            }
-        } else {
-            # Return to the original prompt
-            Confirm-ClearAllCaches
         }
     }
 
@@ -61,11 +26,14 @@ function Clear-BrowserCaches {
         @{Name='Brave'; Path='HKLM:\Software\Microsoft\Windows\CurrentVersion\App Paths\brave.exe'; CachePath="$env:LOCALAPPDATA\BraveSoftware\Brave-Browser\User Data\Default\Cache"},
         @{Name='Internet Explorer'; Path='HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\IEXPLORE.EXE'; CachePath="$env:LOCALAPPDATA\Microsoft\Windows\INetCache"},
         @{Name='Safari'; Path='HKLM:\Software\Apple Inc.\Safari'; CachePath="$env:USERPROFILE\AppData\Local\Packages\AppleInc.Safari_*\AC\Microsoft\Crypto\RSA\S-1-5-21-*\Cache"}
-        # Add more browsers as needed
     )
 
-    Confirm-ClearAllCaches
+    foreach ($browser in $browsers) {
+        Clear-BrowserCache -browserName $browser.Name -cachePath $browser.CachePath
+    }
+
+    Write-Host "All detected browsers cached data has been cleared."
 }
 
-# Clear all browser cache
+# Clear all browser cache without user interaction
 Clear-BrowserCaches
