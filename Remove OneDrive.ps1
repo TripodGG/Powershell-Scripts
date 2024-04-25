bdd190adddd303f9951c0afef350a2627f103585
TripodGG <157990933+TripodGG@users.noreply.github.com>
2024-04-25 13:10:48 -0500

    Update Remove OneDrive.ps1
    set the execution policy, modified text output to allow for colors, added a message regarding the error messages that will likely pop up at the end of removing OneDrive due to the files no longer being there

--- Remove OneDrive.ps1
+++ Remove OneDrive.ps1
@@ -8,12 +8,18 @@
 # Clear the screen
 Clear-Host
 
+# Set the execution policy
+Set-ExecutionPolicy -RemoteSigned
+
+# Inform the user about possible error messages
+Write-Host "Should any error messages pop up during the removal of OneDrive, do not worry. You can ignore them. The script is checking for files after they've been removed. This is expected behavior." -ForegroundColor Blue
+
 # Import modules
 Import-Module -DisableNameChecking $PSScriptRoot\..\lib\force-mkdir.psm1
 Import-Module -DisableNameChecking $PSScriptRoot\..\lib\take-own.psm1
 
 # Stop the OneDrive process
-Write-Output "Stopping OneDrive process..." -ForegroundColor Yellow
+Write-Host "Stopping OneDrive processes..." -ForegroundColor Yellow
 taskkill.exe /F /IM "OneDrive.exe"
 
 # Stop explorer.exe to prevent OneDrive from restarting while being uninstalled
@@ -23,7 +29,7 @@ taskkill.exe /F /IM "explorer.exe"
 Start-Sleep 10
 
 # Uninstall OneDrive
-Write-Output "Uninstalling OneDrive..." -ForegroundColor Cyan
+Write-Host "Uninstalling OneDrive..." -ForegroundColor Cyan
 if (Test-Path "$env:systemroot\System32\OneDriveSetup.exe") {
     & "$env:systemroot\System32\OneDriveSetup.exe" /uninstall
 }
@@ -31,7 +37,7 @@ if (Test-Path "$env:systemroot\SysWOW64\OneDriveSetup.exe") {
     & "$env:systemroot\SysWOW64\OneDriveSetup.exe" /uninstall
 }
 
-Write-Output "Removing residual OneDrive files..." -ForegroundColor Yellow
+Write-Host "Removing residual OneDrive files..." -ForegroundColor Yellow
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:localappdata\Microsoft\OneDrive"
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:programdata\Microsoft OneDrive"
 Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "$env:systemdrive\OneDriveTemp"
@@ -41,12 +47,12 @@ If ((Get-ChildItem "$env:userprofile\OneDrive" -Recurse | Measure-Object).Count 
 }
 
 # Disable OneDrive GPO
-Write-Output "Disabling OneDrive policies via Group Policy" -ForegroundColor Cyan
+Write-Host "Disabling OneDrive policies via Group Policy" -ForegroundColor Cyan
 force-mkdir "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive"
 Set-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive" "DisableFileSyncNGSC" 1
 
 # Remove OneDrive from file explorer
-Write-Output "Removing OneDrive from file explorer sidebar..." -ForegroundColor Yellow
+Write-Host "Removing OneDrive from file explorer sidebar..." -ForegroundColor Yellow
 New-PSDrive -PSProvider "Registry" -Root "HKEY_CLASSES_ROOT" -Name "HKCR"
 mkdir -Force "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}"
 Set-ItemProperty "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" "System.IsPinnedToNameSpaceTree" 0
@@ -55,28 +61,28 @@ Set-ItemProperty "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}
 Remove-PSDrive "HKCR"
 
 # Prevent OneDrive from running under any new users
-Write-Output "Removing run hook for new users..." -ForegroundColor Cyan
+Write-Host "Removing run hook for new users..." -ForegroundColor Cyan
 reg load "hku\Default" "C:\Users\Default\NTUSER.DAT"
 reg delete "HKEY_USERS\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "OneDriveSetup" /f
 reg unload "hku\Default"
 
 # Remove OneDrive from the start menu
-Write-Output "Removing OneDrive's Start menu entry..." -ForegroundColor Yellow
+Write-Host "Removing OneDrive's Start menu entry..." -ForegroundColor Yellow
 Remove-Item -Force -ErrorAction SilentlyContinue "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk"
 
 # Remove OneDrive's scheduled task
-Write-Output "Removing OneDrive scheduled task..." -ForegroundColor Cyan
+Write-Host "Removing OneDrive scheduled task..." -ForegroundColor Cyan
 Get-ScheduledTask -TaskPath '\' -TaskName 'OneDrive*' -ea SilentlyContinue | Unregister-ScheduledTask -Confirm:$false
 
 # Restart explorer.exe
-Write-Output "Restarting explorer..." -ForegroundColor Yellow
+Write-Host "Restarting explorer..." -ForegroundColor Yellow
 Start-Process "explorer.exe"
 
 # Wait for explorer to load
 Start-Sleep 10
 
 # Remove any additional residual files
-Write-Output "Removing any additional residual OneDrive files..." -ForegroundColor Cyan
+Write-Host "Removing any additional residual OneDrive files..." -ForegroundColor Cyan
 foreach ($item in (Get-ChildItem "$env:WinDir\WinSxS\*onedrive*")) {
     Takeown-Folder $item.FullName
     Remove-Item -Recurse -Force $item.FullName
